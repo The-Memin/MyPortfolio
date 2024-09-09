021804f6d337227a537635c169ccd182524d6706
The-Memin <guillejuma@outlook.es>
2024-09-09 13:24:54 -0600

    Mejorando el diseño responsive

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -3,7 +3,7 @@
 export function DateTime({position, company, date, description, companyLink}){
     return(
         <li className='realtive flex pl-8 flex-col gap-5 md:gap-0 md:flex-row md:items-center'>
-            <div className='relative md:w-2/5 lg:p-8 text-start md:text-end flex flex-col gap-1 md:pr-[5rem] text-nowrap before:content-[""] before:absolute before:h-[340%] md:before:h-full before:border-l-2 before:border-black/20 dark:before:border-white/15 before:left-[-1.5em] md:before:left-auto md:before:right-0 pb-8'>
+            <div className='relative md:w-2/5 lg:p-8 text-start md:text-end flex flex-col gap-1 md:pr-[5rem] text-nowrap before:content-[""] before:absolute before:h-[250%] md:before:h-full before:border-l-2 before:border-black/20 dark:before:border-white/15 before:left-[-1.5em] md:before:left-auto md:before:right-0 pb-8'>
                 <h3 className='text-xl font-bold text-sky-400'>{position}</h3>
                 <a href={companyLink} target="_blank" className='font-semibold text-xl text-gray-600 dark:text-white'>{company}</a>
                 <time datetime="">{date}</time>
